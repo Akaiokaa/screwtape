e9363dafa4ba2b869729a919adab05b73a136466
Akai <161771111+Akaiokaa@users.noreply.github.com>
2026-02-02 13:08:11 -0800

    Implemented logic for toList method

--- src/Node.java
+++ src/Node.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -67,6 +68,13 @@ public class Node {
    */
   public List<Integer> toList() {
     // TODO: Implement this
-    return null;
+    List<Integer> list = new ArrayList<>();
+    Node current = this;
+
+    while(current != null){
+      list.add(current.value);
+      current = current.next;
+    }
+    return list;
   }
 }
